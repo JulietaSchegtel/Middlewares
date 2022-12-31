fec15ad91d92b485c9cdf8e700ee075fce34510e
JuliSchegtel <julieta.schegtel@gmail.com>
2022-12-31 15:14:53 -0300

    archivo middle

--- src/middlewares/accesAdmin.js
+++ src/middlewares/accesAdmin.js
@@ -3,9 +3,10 @@ module.exports = (req,res,next) => {
     if (admins.includes(req.query.user)) {
         next()
     }else{
-        res.render('desconocido')
+        res.render('desconocido');
 
-        
+
+
     }
 
 }
--- src/middlewares/userLogs.js
+++ src/middlewares/userLogs.js
@@ -3,9 +3,9 @@ const path = require('path')
 let fecha = new Date
 module.exports = (req,res,next) => {
   fs.appendFileSync(path.join(__dirname,'../logs/userLogs.txt'),'El usuario ingreso a la ruta: '+ req.url/* PATH es lo mismo */ + ' ------ el dia: '+ fecha+ '\n')
-  next()   
+  next()   ;
 
-  
+
 
 }
 
